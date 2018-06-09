6cdf699f86719d5987a582e2548ded84d7d58579
Brendan <brendan_lindsey@outlook.com>
2018-06-09 15:32:34 -0700

    Changed from stdout to print

--- utilities/timer.py
+++ utilities/timer.py
@@ -7,7 +7,7 @@ def countdown(minutes):
     for i in range(minutes*60, 0, -1):
         print(" {}{}:{:0>2d}".format(STDOUT_CLEAR, i//60, i%60), end="\r", flush=True)
         sleep(1)
-    stdout.write("\n")
+    print() # move to next line 
 
 
 def main():
